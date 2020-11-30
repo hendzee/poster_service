38bb2721bad1121e586ee9a38fedf96e8d46f239
Virginia Hendras <hendzcode@gmail.com>
2020-11-30 20:46:43 +0700

    :sparkles: Adding store and update users system

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,26 +9,68 @@ class UserController extends Controller
 {
     /** Get all data user */
     public function index(Request $request) {
-        $user = User::all();
+        try {
+            $user = User::all();
+            $user->email = $request->email;
+            $user->phone = $request->phone;
+            $user->password = $request->password;
+            $user->first_name = $request->first_name;
+            $user->last_name = $request->last_name;
+            $user->photo = $request->photo;
+            $user->country = $request->country;
+            $user->save();
+
+            return $request;
+        } catch (\Throwable $th) {
+            $response['message'] = 'Failed to get data user';
+            
+            return response()->json($response, 500); 
+        }
 
         return $user;
     }
 
     /** Store data user */
     public function store(Request $request) {
-        $user = new User;
-
-        $user->email = $request->email;
-        $user->phone = $request->phone;
-        $user->password = $request->password;
-        $user->first_name = $request->first_name;
-        $user->last_name = $request->last_name;
-        $user->photo = $request->photo;
-        $user->country = $request->country;
+        try {
+            $user = new User;
+    
+            $user->email = $request->email;
+            $user->phone = $request->phone;
+            $user->password = $request->password;
+            $user->first_name = $request->first_name;
+            $user->last_name = $request->last_name;
+            $user->photo = $request->photo;
+            $user->country = $request->country;
+            $user->save();
+    
+            return $request;
+        } catch (\Throwable $th) {
+            $response['message'] = 'Failed to store data';
+            
+            return response()->json($response, 500);
+        }
     }
 
     /** Update data user */
     public function update(Request $request, $id) {
-        $user = User::find($id);
+        try {
+            $user = User::findOrFail($id);
+
+            $user->email = $request->email;
+            $user->phone = $request->phone;
+            $user->password = $request->password;
+            $user->first_name = $request->first_name;
+            $user->last_name = $request->last_name;
+            $user->photo = $request->photo;
+            $user->country = $request->country;
+            $user->save();
+
+            return $request;   
+        } catch (\Throwable $th) {
+            $response['message'] = 'Failed to store data';
+            
+            return response()->json($response, 500);
+        }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,3 +1,5 @@
 <?php
     /** Users route */
     Route::get('users', 'UserController@index');
+    Route::post('users', 'UserController@store');
+    Route::put('users/{id}', 'UserController@update');
