61867d85c610d2ac923585b5e0d9b872a93a8ceb
Virginia Hendras <hendzcode@gmail.com>
2020-12-09 19:37:42 +0700

    Formatting route with 'uses =>'

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,11 @@
 <?php
     /** Users route */
-    Route::get('users', 'UserController@index');
-    Route::post('users', 'UserController@store');
-    Route::put('users/{id}', 'UserController@update');
+    Route::get('users', ['uses' => 'UserController@index']);
+    Route::post('users', ['uses' => 'UserController@store']);
+    Route::put('users/{id}', ['uses' => 'UserController@update']);
 
     /** Posters route */
-    Route::get('posters', 'PosterController@index');
+    Route::get('posters', ['uses' => 'PosterController@index']);
     Route::post('posters', ['uses' => 'PosterController@store']);
     Route::get('posters/{id}', ['uses' => 'PosterController@show']);
     Route::put('posters/{id}', ['uses' => 'PosterController@update']);
