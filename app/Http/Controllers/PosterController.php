6e33e4f5d6d5124d31f236184383c5cb3f2d3937
Virginia Hendras <hendzcode@gmail.com>
2020-12-09 17:37:47 +0700

    Creating index, store and show function for poster controller

--- app/Http/Controllers/PosterController.php
+++ app/Http/Controllers/PosterController.php
@@ -28,7 +28,55 @@ class PosterController extends Controller
                 return $this->getDatabaseErrorResponse($th->errorInfo[1], $th->errorInfo[2]);      
             }
             
-            return $this->simpleResponseError();
+            return $this->simpleErrorResponse();
+        }
+    }
+
+    /** Store poster data */
+    public function store(Request $request) {
+        try {
+            $poster = new Poster;
+
+            $poster->owner = $request->owner;
+            $poster->title = $request->title;
+            $poster->description = $request->description;
+            $poster->price = $request->price;
+            $poster->country = $request->country;
+            $poster->location = $request->location;
+            $poster->detail_location = $request->detail_location;
+            $poster->website = $request->website;
+            $poster->facebook = $request->facebook;
+            $poster->instagram = $request->instagram;
+            $poster->twitter = $request->twitter;
+            $poster->category = $request->category;
+            $poster->save();
+
+            return $this->simpleResponse($poster);
+        } catch (\Throwable $th) {
+            if (property_exists($th, 'errorInfo')) {
+                return $this->getDatabaseErrorResponse($th->errorInfo[1], $th->errorInfo[2]);      
+            }
+
+            return $this->simpleErrorResponse($th);
+        }
+    }
+
+    /** Show specific poster data */
+    public function show($id) {
+        try {
+            $poster = Poster::find($id);
+
+            if (!$poster) {
+                return $this->simpleResponse($poster, 'Data not found');
+            }
+
+            return $this->simpleResponse($poster);
+        } catch (\Throwable $th) {
+            if (property_exists($th, 'errorInfo')) {
+                return $this->getDatabaseErrorResponse($th->errorInfo[1], $th->errorInfo[2]);      
+            }
+            
+            return $this->simpleErrorResponse();
         }
     }
 }
