673a5df8f99ef1d059eef6435dba95452f940476
Virginia Hendras <hendzcode@gmail.com>
2020-12-13 13:51:04 +0700

    Adding additional information comments route

--- routes/api.php
+++ routes/api.php
@@ -1,16 +1,17 @@
 <?php
     /** Users route */
-    Route::get('users', ['uses' => 'UserController@index']);
-    Route::get('users/posters', ['uses' => 'UserController@getUserPoster']);
-    Route::post('users', ['uses' => 'UserController@store']);
-    Route::put('users/{id}', ['uses' => 'UserController@update']);
+    Route::get('users', ['uses' => 'UserController@index']); // Get user
+    Route::get('users/posters', ['uses' => 'UserController@getUserPoster']); // Get user's poster
+    Route::get('users/subscriptions', ['uses' => 'UserController@getUserSubscription']); // Get user's subscription
+    Route::post('users', ['uses' => 'UserController@store']); // Store new user
+    Route::put('users/{id}', ['uses' => 'UserController@update']); // Update user by id
 
     /** Posters route */
-    Route::get('posters', ['uses' => 'PosterController@index']);
-    Route::get('posters/trending', ['uses' => 'PosterController@getTrendingPoster']);
-    Route::get('posters/recommendation', ['uses' => 'PosterController@getRecommendationPoster']);
-    Route::post('posters', ['uses' => 'PosterController@store']);
-    Route::get('posters/{id}', ['uses' => 'PosterController@show']);
-    Route::put('posters/{id}', ['uses' => 'PosterController@update']);
-    Route::delete('posters/{id}', ['uses' => 'PosterController@destroy']);
+    Route::get('posters', ['uses' => 'PosterController@index']); // Get poster
+    Route::get('posters/trending', ['uses' => 'PosterController@getTrendingPoster']); // Get trending poster
+    Route::get('posters/recommendation', ['uses' => 'PosterController@getRecommendationPoster']); // Get recommendation poster
+    Route::post('posters', ['uses' => 'PosterController@store']); // Store new poster
+    Route::get('posters/{id}', ['uses' => 'PosterController@show']); // Show detail poster
+    Route::put('posters/{id}', ['uses' => 'PosterController@update']); // Update poster by id
+    Route::delete('posters/{id}', ['uses' => 'PosterController@destroy']); // Delete poster by id
     
